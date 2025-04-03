824839fae99f202466336c98c18cc00d41aaf7aa
Tanvi Gupta <guptanvi7623@gmail.com>
2025-04-03 19:03:52 +0530

    //Splash Screen created

---
+++ app/src/main/java/com/angel6677/wavesoffood/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.angel6677.wavesoffood
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java) // Correct variable naming
+            startActivity(intent)
+            finish()
+        }, 7000) // Correct syntax
+    }
+}
