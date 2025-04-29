5846b860b06fe22d9ad66c293e7b68b21c884eb7
Tanvi Gupta <guptanvi7623@gmail.com>
2025-04-29 16:08:24 +0530

    updation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,10 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    android {
-        buildFeatures {
-            viewBinding = true
-        }
+
+    buildFeatures {
+        viewBinding = true  // Enables view binding
     }
 
     buildTypes {
@@ -31,23 +30,27 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
 }
---
+++ app/src/main/java/com/angel6677/wavesoffood/ChooseLocationActivity.kt
@@ -0,0 +1,29 @@
+package com.angel6677.wavesoffood
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.angel6677.wavesoffood.databinding.ActivityChooseLocationBinding
+
+class ChooseLocationActivity : AppCompatActivity() {
+    private val binding:ActivityChooseLocationBinding by lazy{
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        val locationList:Array<String> =arrayOf("Agra","Delhi","Mumbai","Gurgaon","Bengaluru")
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,locationList)
+        val autoCompleteTextView=binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
